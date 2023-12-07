8345cafdcfe93abfe3c927117810711f150ff96a
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2023-12-07 18:27:31 +0100

    adding photos works

--- pages/car/[id].vue
+++ pages/car/[id].vue
@@ -7,10 +7,16 @@ const authStore = useAuthStore();
 
 const route = useRoute();
 
-onMounted(async () => {
+const isAddingCarPhoto = ref(false);
+
+async function fetchCurrentCar() {
   if (authStore.user != null) {
     await carStore.fetchCurrentCarByCarID(route.params.id as unknown as number);
   }
+}
+
+onMounted(async () => {
+  await fetchCurrentCar();
 });
 </script>
 
@@ -29,12 +35,31 @@ onMounted(async () => {
     </div>
 
     <div class="flex overflow-auto gap-10 p-10 rounded-3xl bg-background-color">
+      <div
+        class="flex flex-col justify-center self-center w-32 h-32 rounded-3xl bg-overlay-background-color group"
+      >
+        <Button
+          class="w-32 text-5xl text-center duration-300 group-hover:text-hover-color"
+          @click="() => (isAddingCarPhoto = true)"
+        >
+          
+        </Button>
+      </div>
       <img
-        class="h-32"
-        v-for="photo in carStore.currentCar.photos"
+        class="h-32 rounded-3xl shadow-md"
+        v-for="photo in carStore.currentCar.photos?.toReversed()"
         :src="`//localhost:8000/uploads/car_photos/${photo.content}`"
       />
     </div>
+    <AddCarPhotoPanel
+      @submit="
+        () => {
+          isAddingCarPhoto = false;
+          fetchCurrentCar();
+        }
+      "
+      v-if="isAddingCarPhoto"
+    />
   </div>
   <div v-else>This car dosen't exist!</div>
 </template>
